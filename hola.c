8be236572979ea4f3ec6a4ca1750d2089834dc9b
Rubén Burgué <err0r@debian.err0r>
2014-05-19 18:14:13 +0200

    Añadida información adicional. 	printf

--- hola.c
+++ hola.c
@@ -6,5 +6,6 @@ int main (int argc,char **argv)
    	printf("Hola mi nombre es Rubén Burgué Pérez\n");
    	printf("Tengo 22 años\n");
 	printf("Vivo en A Coruña");
+	printf("Estudio Grado en Ingeniería informática");
 	return 0;
 }
