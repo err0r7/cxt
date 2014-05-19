de03c47163a54211e6a3edb2245d67b90df7f1c7
Rubén Burgué <err0r@debian.err0r>
2014-05-19 18:20:23 +0200

    Añadida info extra con respecto a la edad.

--- hola.c
+++ hola.c
@@ -4,7 +4,7 @@ int main (int argc,char **argv)
 {
 	/*Esto es un simple comentario*/
    	printf("Hola mi nombre es Rubén Burgué Pérez\n");
-   	printf("Tengo 22 años\n");
+   	printf("Tengo 22 años dado que nací en 1992\n");
 	printf("Vivo en A Coruña");
 	printf("Estudio Grado en Ingeniería informática\n");
 	printf("Pero en realidad soy de la provincia de Lugo\n");
