9c75fcc63e3e204854ee59df533360fbd3e5df7d
Rubén Burgué <err0r@debian.err0r>
2014-05-19 18:13:05 +0200

    Añadido un comentario aclarativo

--- hola.c
+++ hola.c
@@ -2,6 +2,7 @@
  
 int main (int argc,char **argv)
 {
+	/*Esto es un simple comentario*/
    	printf("Hola mi nombre es Rubén Burgué Pérez\n");
    	printf("Tengo 22 años\n");
 	printf("Vivo en A Coruña");
